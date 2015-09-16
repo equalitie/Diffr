79e098ab3e0991dcf95be4d1caf2e5c6735c8966
Zack Mullaly <redwire@riseup.net>
2015-09-16 17:54:26 -0400

    Wrote wrappers around phantomjs stuff

---
+++ errortesting.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * A set of helper functions to interact with the phantom library using promises
+ * instead of the standard callback-based approach.
+ */
+
+function phCreate() {
+  return new Promise((resolve, reject) =>
+    phantom.create(phantomInst => resolve(phantomInst)));
+}
+
+function phCreatePage(phantomInstance) {
+  return new Promise((resolve, reject) =>
+    phantomInstance.createPage(page => resolve(page)));
+}
+
+function phPageOpen(page, url) {
+  return new Promise((resolve, reject) =>
+    // TODO - May want to also pass page through the resolver
+    page.open(url, stat => resolve(stat)));
+}
+
+function phPageOnError(page, errHandler) {
+  return new Promise((resolve, reject) =>
+    page.onError(err => resolve(errHandler(err))));
+}
+
+module.exports = {
+  phCreate,
+  phCreatePage,
+  phPageOpen,
+  phPageOnError
+};
